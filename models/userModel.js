9d04ef7df5eaddf2be6da4709a0da4995711f5a9
kpps17 <sinhaarchit17@gmail.com>
2021-09-21 18:06:46 +0530

    made user schema

--- models/userModel.js
+++ models/userModel.js
@@ -1,11 +1,54 @@
 const mongoose = require("mongoose");
 const {dbLink} = require("../secrets/links")
 
-console.log(dbLink);
+// console.log(dbLink);
 
-mongoose.connect(dbLink).then((db) => {
+mongoose.connect(db_link).then(function (db) {
+    // console.log(db);
     console.log('db connected');
-    console.log(db);
-}).catch((err) => {
-    console.log(err);
+})
+    .catch(function (err) {
+        console.log(err);
+    });
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: function () {
+            return validator.validate(this.email);
+        }
+    },
+    createdAt: {
+        type: String,
+    },
+    password: {
+        type: String,
+        required: true,
+        min: 8
+    },
+    confirmPassword: {
+        type: String,
+        required: true,
+        min: 8,
+        validate: function () {
+            return this.password == this.confirmPassword
+        }
+    }
 });
+
+userSchema.pre('save', function () {
+    this.confirmPassword = undefined;
+})
+
+const userModel = mongoose.model('userModel', userSchema);
+
+module.exports = userModel;
