7c9d76d6012be0453ddd41c4798c8c4da1b88370
kpps17 <sinhaarchit17@gmail.com>
2021-10-10 00:24:26 +0530

    sending email to new login

--- Routers/authRouter.js
+++ Routers/authRouter.js
@@ -3,27 +3,29 @@ const authRouter = express.Router();
 const path = require('path')
 const { userModel } = require('../models/userModel');
 const jwt = require('jsonwebtoken');
-const {JWT_KEY} = require('../secrets/links');
+const { JWT_KEY } = require('../secrets/links');
+const sendMail = require('../nodemailer');
 
 authRouter.route('/signup').post(setCreatedAt, async (req, res) => {
-	try {
-		let userDetails = req.body;
-		console.log(req.body);
-		// user.push(userDetails);
-		let user = await userModel.create(userDetails);
-		res.json({
-			message: "user signed up",
-			user: userDetails,
-		})
-		// console.log(user)
-	} catch (err) {
-		console.log(err);
-	}
+    try {
+        let userDetails = req.body;
+        console.log(req.body);
+        // user.push(userDetails);
+        let user = await userModel.create(userDetails);
+        sendMail(user);
+        res.json({
+            message: "user signed up",
+            user: userDetails,
+        })
+        // console.log(user)
+    } catch (err) {
+        console.log(err);
+    }
 })
 
 authRouter.route('/forgotPassword').get((req, res) => {
-	let indexPath = path.join(__dirname, "../public/forgotPassword.html");
-	res.sendFile(indexPath);
+    let indexPath = path.join(__dirname, "../public/forgotPassword.html");
+    res.sendFile(indexPath);
 })
 authRouter.route('/login').post(loginUser)
 
@@ -31,47 +33,47 @@ authRouter.route('/login').post(loginUser)
 
 
 function setCreatedAt(req, res, next) {
-	// console.log('in setCreated at');
-	let obj = req.body;
-	let len = Object.keys(obj).length;
-	if (len == 0) return res.status(400).json({ message: "cannot create if body is empty" });
-	req.body.createdAt = new Date().toISOString();
-	next();
+    // console.log('in setCreated at');
+    let obj = req.body;
+    let len = Object.keys(obj).length;
+    if (len == 0) return res.status(400).json({ message: "cannot create if body is empty" });
+    req.body.createdAt = new Date().toISOString();
+    next();
 }
 
 async function loginUser(req, res) {
-	try {
-		if (req.body.email) {
-			let user = await userModel.findOne({ email: req.body.email });
-			if (user) {
-				if (req.body.password == user.password) {
-					let payload = user['_id'];
-					let token = jwt.sign({id: payload}, JWT_KEY);
-					console.log(token);
-					res.cookie('login', token, {httpOnly: true});
-					return res.json({
-						message: "user logged in",
-					})
-				} else {
-					return res.json({
-						message: "email or password is wrong",
-					})
-				}
-			} else {
-				return res.json({
-					message: "email or password is wrong",
-				});
-			}
-		} else {
-			return res.json({
-				message: "user is not present",
-			})
-		}
-	} catch (err) {
-		return res.status(500).json({
-			message: err.message,
-		});
-	}
+    try {
+        if (req.body.email) {
+            let user = await userModel.findOne({ email: req.body.email });
+            if (user) {
+                if (req.body.password == user.password) {
+                    let payload = user['_id'];
+                    let token = jwt.sign({ id: payload }, JWT_KEY);
+                    console.log(token);
+                    res.cookie('login', token, { httpOnly: true });
+                    return res.json({
+                        message: "user logged in",
+                    })
+                } else {
+                    return res.json({
+                        message: "email or password is wrong",
+                    })
+                }
+            } else {
+                return res.json({
+                    message: "email or password is wrong",
+                });
+            }
+        } else {
+            return res.json({
+                message: "user is not present",
+            })
+        }
+    } catch (err) {
+        return res.status(500).json({
+            message: err.message,
+        }); 
+    }
 }
 
 module.exports = { authRouter };
--- nodemailer.js
+++ nodemailer.js
@@ -1,27 +1,33 @@
+const { model } = require('mongoose');
 const nodemailer = require('nodemailer');
 const { email, password } = require('./secrets/links');
 
-let transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    auth: {
-        user: email, // generated ethereal user
-        pass: password, // generated ethereal password
-    },
-});
+module.exports = async function sendMail(user) {
+    let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: email, // generated ethereal user
+            pass: password, // generated ethereal password
+        },
+    });
+    
+    let clientEmail = user.email;
 
-let mailOptions = {
-    from : `${email}`,
-    to : "",
-    subject: "testing",
-    text: "amazing😉"
+    let mailOptions = {
+        from : `"Food App"${email}`,
+        to : `${clientEmail}`,
+        subject: "testing",
+        text: "amazing😉",
+        html: "<h1> Hii welcome to a new World " 
+    };
+    
+    transporter.sendMail(mailOptions, (err, data) => {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log('sucess');
+        }
+    });
 };
-
-transporter.sendMail(mailOptions, (err, data) => {
-    if(err) {
-        console.log(err);
-    } else {
-        console.log('sucess');
-    }
-});
